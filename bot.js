b040e947a6997997b51f7478233575a14d76f1e7
Robben9874 <37851177+Robben9874@users.noreply.github.com>
2018-11-03 19:49:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ A7MD.on("guildMemberAdd", member => {
 **
 سلام عليكم 
 لو سمحت ممكن تدخل سيرفرى
-                               [ https://discord.gg/ArUtZJj ] 
+                               [ https://discord.gg/tKxjph8 ] 
 الدعووة خاصة لك ... [ ${member}  ]
 **`) 
 }).catch(console.error)
